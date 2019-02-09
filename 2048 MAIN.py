accdfb91f3f07e100114b35c3d2432cedba31ef5
JiHwan Shin <46997797+shinben0327@users.noreply.github.com>
2019-02-09 23:24:12 +0900

    2048 MAIN draft#8
    All of the up() down() left() right() functions are complete without any errors!

--- 2048 MAIN.py
+++ 2048 MAIN.py
@@ -11,21 +11,10 @@ This program recreates 2048. Notes for this project will be written here
 
 https://en.wikipedia.org/wiki/2048_(video_game)#Gameplay
 
-FUNCTIONS TO MAKE
-initialise(), V
-show(), V
-move(), 
-up(),
-down(),
-left(), V
-right()
-check2048() V
-
-while not check2048(board):
-    show()
-    move()
-    
-could add score system after finishing 
+STUFF TO DO
+make scoring system
+rewrite  checkgameover()
+make sure that rows and columns match even with big numbers
 """
 
 print("* Hi! This is a recreation of the game 2048 on Python. Have fun! *")  # starting message
@@ -38,33 +27,73 @@ def show():
 
 
 def up():
-    pass
+    # this recreates the board into the column point of view
+    columns = [[], [], [], []]
+    for i in range(4):
+        for j in range(4):
+            columns[i].append(board[j][i])
+    for column in columns:
+        # this moves all the numbers
+        counter = 0
+        for i in range(3, -1, -1):
+            if column[i] == 0:
+                column.remove(column[i])
+                counter += 1
+        # this combines the numbers if they are identical
+        for i in range(len(column)-1):
+            if column[i] == column[i+1]:
+                column[i] *= 2
+                del column[i+1]
+                column.append(0)
+        # this makes sure that the row still has four numbers
+        for i in range(counter):
+            column.append(0)
+    # this turns the columns back into the board
+    for i in range(4):
+        for j in range(4):
+            board[i][j] = columns[j][i]
 
 
 def down():
-    pass
+    # this recreates the board into the column point of view
+    columns = [[], [], [], []]
+    for i in range(4):
+        for j in range(4):
+            columns[i].append(board[j][i])
+    for column in columns:
+        # this moves all the numbers
+        counter = 0
+        for i in range(3, -1, -1):
+            if column[i] == 0:
+                column.remove(column[i])
+                counter += 1
+        # this combines the numbers if they are identical
+        for i in range(len(column) - 1, 0, -1):
+            if column[i] == column[i - 1]:
+                column[i] *= 2
+                del column[i - 1]
+                column.insert(0, 0)
+        # this makes sure that the row still has four numbers
+        for i in range(counter):
+            column.insert(0, 0)
+    # this turns the columns back into the board
+    for i in range(4):
+        for j in range(4):
+            board[i][j] = columns[j][i]
 
 
 def left():
     for row in board:
         # this moves all the numbers
         counter = 0
-        if row[3] == 0:
-            row.remove(row[3])
-            counter += 1
-        if row[2] == 0:
-            row.remove(row[2])
-            counter += 1
-        if row[1] == 0:
-            row.remove(row[1])
-            counter += 1
-        if row[0] == 0:
-            row.remove(row[0])
-            counter += 1
+        for i in range(3, -1, -1):
+            if row[i] == 0:
+                row.remove(row[i])
+                counter += 1
         # this combines the numbers if they are identical
         for i in range(len(row)-1):
             if row[i] == row[i+1]:
-                row[i] = row[i]*2
+                row[i] *= 2
                 del row[i+1]
                 row.append(0)
         # this makes sure that the row still has four numbers
@@ -73,7 +102,22 @@ def left():
 
 
 def right():
-    pass
+    for row in board:
+        # this moves all the numbers
+        counter = 0
+        for i in range(3, -1, -1):
+            if row[i] == 0:
+                row.remove(row[i])
+                counter += 1
+        # this combines the numbers if they are identical
+        for i in range(len(row)-1, 0, -1):
+            if row[i] == row[i-1]:
+                row[i] *= 2
+                del row[i-1]
+                row.insert(0, 0)
+        # this makes sure that the row still has four numbers
+        for i in range(counter):
+            row.insert(0, 0)
 
 
 def gamequit():
@@ -88,9 +132,9 @@ def newrandnum():
             break
         else:
             continue
-    twofour = random.choices(  # this will randomly choose the new number that will appear
-        population=[2, 4],  # 75% of the time it will be 2, 25% of the time it will be 4
-        weights=[0.75, 0.25]
+    twofour = random.choices(
+        population=[2, 4],  # this will randomly choose the new number that will appear
+        weights=[0.75, 0.25]  # 75% of the time it will be 2, 25% of the time it will be 4
     )
     board[x][y] = twofour[0]
 
@@ -110,7 +154,7 @@ def move(direction=input("Please type 'up' / 'down' / 'left' / 'right' and press
     newrandnum()
 
 
-def checkgameover():  # this checks if the player's board is full
+def checkgameover():  # this checks if the player lost the game
     numbersinboard = []
     for row in board:
         for number in row:
@@ -124,17 +168,16 @@ def check2048():  # this checks if 2048 is in the board
     for row in board:
         if 2048 in row:
             return True
-            break
     return False
 
 
-while not check2048():  # the game-play repeats until the player gets either GAMEOVER or 2048
+while not checkgameover() and not check2048():  # the game-play repeats until the player gets either GAMEOVER or 2048
     show()
     move(direction=input("Please type 'up' / 'down' / 'left' / 'right' and press enter: "))
-    if checkgameover():
-        show()
-        print("GAMEOVER")
-        break
+
+if checkgameover():
+    show()
+    print("GAMEOVER")
 
 if check2048():
     show()
